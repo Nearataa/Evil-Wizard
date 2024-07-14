9badad0ec1b7ac37f666e085e3e7430f38d93dad
Sascha Novak <sn062@hdm-stuttgart.de>
2024-07-14 12:55:02 +0200

    Stuff

--- Assets/Scenes/PortalTrigger.cs
+++ Assets/Scenes/PortalTrigger.cs
@@ -5,11 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class PortalTrigger : MonoBehaviour
 {
+    [SerializeField] bool goNextLevel;
+    [SerializeField] string levelName;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            GameManager.Instance.LoadNextScene();
+            UnlockNewLevel();
+            GameManager.Instance.NextLevel();
+        }
+    }
+    void UnlockNewLevel()
+    {
+        if (SceneManager.GetActiveScene().buildIndex >= PlayerPrefs.GetInt("ReachedIndex"))
+        {
+            PlayerPrefs.SetInt("ReachedIndex", SceneManager.GetActiveScene().buildIndex + 1);
+            PlayerPrefs.SetInt("UnlockedLevel", PlayerPrefs.GetInt("UnlockedLevel", 1) + 1);
+            PlayerPrefs.Save();
         }
     }
 }
