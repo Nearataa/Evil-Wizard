c32b3be7191598a57b589542f7b5efcfd1d466e8
Sascha Novak <sn062@hdm-stuttgart.de>
2024-07-14 18:53:16 +0200

    enemy ani & quit

--- Assets/Scripts/Enemys/EnemyHealth.cs
+++ Assets/Scripts/Enemys/EnemyHealth.cs
@@ -8,10 +8,12 @@ public class EnemyHealth : MonoBehaviour, IDamageable
 
     private float currentHealth;
 
+    private Animator animator;
     public bool HasTakeDamage { get; set; }
 
     private void Start()
     {
+        animator = GetComponent<Animator>();
         currentHealth = maxHealth;
     }
 
@@ -19,6 +21,7 @@ public class EnemyHealth : MonoBehaviour, IDamageable
     {
         HasTakeDamage = true;
 
+        animator.SetTrigger("hit");
         currentHealth -= damageAmount;
 
         if (currentHealth <= 0)
@@ -29,6 +32,13 @@ public class EnemyHealth : MonoBehaviour, IDamageable
 
     public void Die()
     {
+        StartCoroutine(Kill(0.6f));
+    }
+
+    IEnumerator Kill(float duration)
+    {
+        animator.SetTrigger("death");
+        yield return new WaitForSeconds(duration);
         Destroy(gameObject);
     }
 }
